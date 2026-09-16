d0d109b7eebf6e45e6f0d01604e5877ca6657319
Amrryzen <alvinnoval001@gmail.com>
2026-09-16 16:45:42 +0700

    update 0.0.2
    game.js

--- game.js
+++ game.js
@@ -1,8 +1,16 @@
-const canvas =
-document.getElementById("gameCanvas");
+const canvas = document.getElementById("gameCanvas");
 const ctx = canvas.getContext("2d");
-ctx.fillStyle = "green";
-ctx.fillRect(45, 430, 10, 40);
-const ctxe = canvas.getContext("2d");
-ctxe.fillStyle = "red";
-ctxe.fillRect(420, 400, 30, 70);
+
+const player = {
+  x: 45, y: 430, w: 10, h: 40, color: "green"
+};
+
+const enemy = {
+  x: 420, y: 400, w: 30, h: 70, color: "red"
+};
+
+ctx.fillStyle = player.color;
+ctx.fillRect(player.x, player.y, player.w, player.h);
+
+ctx.fillStyle = enemy.color;
+ctx.fillRect(enemy.x, enemy.y, enemy.w, enemy.h);
