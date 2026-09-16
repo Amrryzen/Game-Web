f34c3088b73ff7f932391e8182f77c612087d396
Amrryzen <alvinnoval001@gmail.com>
2026-09-16 16:31:03 +0700

    Update 0.0.1

---
+++ game.js
@@ -0,0 +1,8 @@
+const canvas =
+document.getElementById("gameCanvas");
+const ctx = canvas.getContext("2d");
+ctx.fillStyle = "green";
+ctx.fillRect(45, 430, 10, 40);
+const ctxe = canvas.getContext("2d");
+ctxe.fillStyle = "red";
+ctxe.fillRect(420, 400, 30, 70);
